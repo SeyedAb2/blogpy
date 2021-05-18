0192984ee0c4799b72e7bf1f46f65dea8bc793d3
SeyedAb2 <tiggerwitht1378@gmail.com>
2021-05-18 20:10:57 +0430

    contact and about page

--- blogpy/blog/urls.py
+++ blogpy/blog/urls.py
@@ -4,5 +4,7 @@ from django.urls import path , re_path
 from blog import views
 
 urlpatterns = [
-    re_path(r'^$', views.IndexPage.as_view(),name = 'index')
+    re_path(r'^$', views.IndexPage.as_view(),name = 'index'),
+    re_path(r'^contact/$', views.ContactPage.as_view(),name = 'contact'),
+    re_path(r'^about/$', views.AboutUs.as_view(),name = 'about'),
 ]
